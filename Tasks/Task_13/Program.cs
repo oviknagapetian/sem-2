7371095e28174e5ee37c5fe2519bb7cc96e18633
Hovik Nahapetian <hoviknahapetian@iMac-Hovik.local>
2023-03-29 01:10:50 +0300

    задача 13 решена

---
+++ Tasks/Task_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Введите число: ");
+        long number = long.Parse(Console.ReadLine());
+
+        // Вычисляем третью цифру числа
+        long thirdDigit = (number / 100) % 10;
+
+        if (thirdDigit == 0) {
+            Console.WriteLine("Третьей цифры нет");
+        } else {
+            Console.WriteLine($"Третья цифра числа: {thirdDigit}");
+        }
+    }
+}
