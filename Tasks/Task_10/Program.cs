a9be1f8369765a8e5e7b763474d9968fe1a2ba74
Hovik Nahapetian <hoviknahapetian@iMac-Hovik.local>
2023-03-29 00:49:57 +0300

    первая задача решена

---
+++ Tasks/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите трёхзначное число:");
+        int number = int.Parse(Console.ReadLine()); // получаем число от пользователя
+
+        int secondDigit = (number / 10) % 10; // вычисляем вторую цифру числа
+
+        Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
+        Console.ReadKey(); // ожидаем нажатия клавиши для завершения программы
+    }
+}
